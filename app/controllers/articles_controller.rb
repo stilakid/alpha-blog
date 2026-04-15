7b6fc5a33ebedf6d99dd09f7097cb3aafe7a68bc
Juben Rana <stilakid@stanford.edu>
2026-04-15 17:45:45 +0600

    Add error msg, page redirection, and success flash msg

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,18 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
-
   def create
-
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      # redirect_to article_path(@article)
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 end
 
