416e513410f31285a5d3d037798169ba8b8e360e
Juben Rana <stilakid@stanford.edu>
2026-04-20 10:13:40 +0600

    Add one-to-many assocaition between users and articles from back end

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+  has_many :articles
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 25}
-  validates :email, format: { with: VALID_EMAIL_REGEX }, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: {minimum: 3, maximum: 25}
+  validates :email,
+            format: { with: VALID_EMAIL_REGEX },
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 }
+
 end
 
