9c215b356bdb282898c9078577a01f577bcab31a
Juben Rana <stilakid@stanford.edu>
2026-04-20 12:52:44 +0600

    Add users create action + home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+  get "about", to: "pages#about"
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :articles
   get 'signup', to: 'users#new'
