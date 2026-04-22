24b43bb7901e214ada65b2c9959eb74ca5b00096
Juben Rana <stilakid@stanford.edu>
2026-04-22 15:50:51 +0600

    Added same user verification for individual user resource management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,11 @@ class ApplicationController < ActionController::Base
   def logged_in?
     !!current_user
   end
+
+  def require_user
+    unless logged_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
 end
