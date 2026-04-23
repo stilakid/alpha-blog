cf7f578bf785280d1d48ffd652d37ed3c30b79fa
Juben Rana <stilakid@stanford.edu>
2026-04-23 16:40:02 +0600

    Add new category creation form and actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   def new
-
+    @category = Category.new
   end
 
   def index
@@ -10,5 +10,24 @@ class CategoriesController < ApplicationController
   def show
 
   end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+
+      flash[:alert] = "The following errors prevented the category from being saved: " + @category.errors.full_messages.join('<br>').html_safe
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 
 end
