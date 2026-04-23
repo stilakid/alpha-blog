02b8669b67f2e1a81bd50fa3583c5805febb6dab
Juben Rana <stilakid@stanford.edu>
2026-04-23 13:35:14 +0600

    Add initial minitest

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_23_061304) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_23_072330) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at"
     t.text "description"
     t.string "title"
     t.datetime "updated_at"
     t.integer "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
