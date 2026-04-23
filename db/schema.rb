adc22da36331ece5843db435acde405d3e78da0a
Juben Rana <stilakid@stanford.edu>
2026-04-23 12:53:36 +0600

    Added admin privileges

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_20_045619) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_23_061304) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at"
     t.text "description"
@@ -20,6 +20,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_20_045619) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.string "email"
     t.string "password_digest"
